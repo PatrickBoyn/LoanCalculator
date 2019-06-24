c9abe3677550da60031e66b0b6e41974ec65504f
Patrick Boynton <dakilangmanok@gmail.com>
2019-06-24 13:49:04 -0400

    Added the calculateResults functions that includes the first three dom variables.  Also prevented the default behavior.

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+function calculateResults(e){
+    console.log('Calculating...');
+
+    // UI variables
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    
+    e.preventDefault();
+}
