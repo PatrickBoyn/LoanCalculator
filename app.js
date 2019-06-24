0852b8401cd6eae2fa0b5c5d69d74c64b3c85369
Patrick Boynton <dakilangmanok@gmail.com>
2019-06-24 13:54:29 -0400

    Added the remaining UI variables.

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ function calculateResults(e){
     const amount = document.getElementById('amount');
     const interest = document.getElementById('interest');
     const years = document.getElementById('years');
-    
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
     e.preventDefault();
 }
